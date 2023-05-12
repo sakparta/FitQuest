99978d0b8c85929307ceb1240c5ff25a545adbd9
WalMaa <98175031+WalMaa@users.noreply.github.com>
2023-05-12 08:27:13 +0300

    Final(?) version of  UI  -WM
    There might exist some conflicts with the charts since I changed them to LineCharts from BarCharts

--- app/assets/screens/ProgressScreen.js
+++ app/assets/screens/ProgressScreen.js
@@ -1,89 +1,102 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Button} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import Icon2 from 'react-native-vector-icons/Entypo';
 import ProgressCircle from 'react-native-progress-circle'
-import { BarChart } from 'react-native-chart-kit';
+import { LineChart } from 'react-native-chart-kit';
 import * as Progress from 'react-native-progress';
-const barData = {
-    labels: ['Start', 'Week 1', 'week 2', 'week 3'],
-    datasets: [
-      {
-        data: [100, 98, 96, 95],
-      },
-    ],
-  };
-  //barchartin style
-  const chartConfig = {
-    backgroundGradientFrom: 'grey',
-    backgroundGradientTo: 'grey',
-    fillShadowGradientOpacity: 1,
-    backgroundGradientToOpacity: 0.5,
-    color: (opacity = 6) => `rgba(0, 0, 255, ${opacity})`,
-    strokeWidth: 3, // optional, default 3
-    barPercentage: 1,
-    useShadowColorFromDataset: false, // optional
-    barRadius: 1,
-   
-  };
+const data = {
+  labels: ['Start', 'Week 1', 'Week 2', 'Week 3'],
+  datasets: [
+    {
+      data: [100, 98, 96, 95],
+    },
+  ],
+};
+//Linechartin style
+const chartConfig = {
+  backgroundColor: "#8cbbf1",
+  backgroundGradientFrom: "#bdbdc7",
+  backgroundGradientTo: "#bdbdc7",
+  decimalPlaces: 2, // optional, defaults to 2dp
+  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  style: {
+    borderRadius: 40
+  },
+  propsForDots: {
+    r: "6",
+    strokeWidth: "2",
+    stroke: "#8cbbf1"
+  }
+};
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.content}>
         <View style={styles.TopContent}>
-            <ProgressCircle
+          <ProgressCircle
             percent={50}
             radius={70}
             borderWidth={30}
-            color="#10e410"
+            color="#73d673"
             shadowColor="#d6d4d4"
-            bgColor="#fff"
-            >
+            bgColor="#ececea"
+          >
             <Text style={{ fontSize: 25 }}>{'50%'}</Text>
-        </ProgressCircle>
+          </ProgressCircle>
         </View>
-        <View style={styles.MidContent}>
-          <View style={styles.Chart}>
-          <BarChart
-            data={barData}
-            width={330}
-            height={200}
-            chartConfig={chartConfig}
-            withInnerLines={true}
-            withHorizontalLabels={true}
-            fromZero={true}
-            showBarTops={false}
-          />
+        <View>
+          <Text style={styles.titleText}>Weight Goal</Text>
         </View>
-        </View>
-          <View style={styles.BottomContent}>
-            <View style={styles.BottomGoalBar}>
-                    <Text style={styles.StartWeightText}>100kg</Text>
-                    <Progress.Bar progress={0.4} width={200} height={50} color={'green'} unfilledColor={'#D3D3D3'}/>
-                    <Text style={styles.EndWeightText}>80kg</Text>
+        <View style={[{ flexDirection: 'row' }, { alignItems: 'center' }]}>
+          <TouchableOpacity>
+            <Icon2 name='chevron-left' size={40} color={'#505050'} />
+          </TouchableOpacity>
+          <View style={styles.MidContent}>
+            <View style={styles.chart}>
+              <LineChart
+                data={data}
+                chartConfig={chartConfig}
+                bezier
+                height={250}
+                width={300}
+              />
             </View>
-            <View style={styles.bottomContentButtons}>
-                <TouchableOpacity style={styles.bottomContentAddButton}>
-                    <Text style={styles.bottomContentButtonText}> ADD GOAL</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.bottomContentEditButton}>
-                   <Text  style={styles.bottomContentButtonText}> EDIT GOAL</Text>
-                </TouchableOpacity>
-            </View>  
+          </View>
+          <TouchableOpacity>
+            <Icon2 name='chevron-right' size={40} color={'#505050'} />
+          </TouchableOpacity>
         </View>
-        
+        <View style={styles.BottomContent}>
+          <View style={styles.BottomGoalBar}>
+            <Text style={styles.barChartText}>100kg</Text>
+            <Progress.Bar progress={0.4} width={200} height={50} color={'#73d673'} borderRadius={10} borderWidth={3} borderColor='#bdbdc7' unfilledColor={'#bdbdc7'} />
+            <Text style={styles.barChartText}>80kg</Text>
+          </View>
+          <View style={styles.bottomContentButtons}>
+            <TouchableOpacity style={styles.bottomContentButton}>
+              <Text style={styles.bottomContentButtonText}> Add Goal</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.bottomContentButton}>
+              <Text style={styles.bottomContentButtonText}> Edit Goal</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
       </View>
       <View style={styles.navBar}>
         <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate("Home")}>
-          <Icon name="home-outline" size={30} color="black"  />
+          <Icon name="home-outline" size={30} color="#505050" />
           <Text style={styles.navButtonText}>Home</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate("Workouts")}>
-          <Icon name="fitness-outline" size={30} color="black" />
+          <Icon name="fitness-outline" size={30} color="#505050" />
           <Text style={styles.navButtonText}>Workouts</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate("Meals")}>
-          <Icon name="fast-food-outline" size={30} color="black" />
+          <Icon name="fast-food-outline" size={30} color="#505050" />
           <Text style={styles.navButtonText}>Meals</Text>
         </TouchableOpacity>
       </View>
@@ -91,105 +104,100 @@ const HomeScreen = ({navigation}) => {
   );
 };
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-      },
-      content: {
-        flex: 1,
-        alignItems: 'center',
-      },
-      navBar: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        height: 80,
-        borderTopWidth: 1,
-        borderTopColor: '#ccc',
-      },
-      navButton: {
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      navButtonText: {
-        fontSize: 12,
-        color: 'black',
-        marginTop: 5,
-      },
-      TopContent:{
-        marginTop: 60,
-        flex: 0.7,
-        marginBottom: 5,
-        width: "100%",
-        alignItems: 'center',
-      },
-      MidContent:{
-        flex: 1.1,
-        backgroundColor: "grey",
-        borderRadius: 20,
-        width: '97%',
-        marginBottom: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderWidth: 2,
-      },
-      Chart:{
-        marginTop: 40,
-      },
-      BottomContent:{
-        flex: 0.8,
-        
-      },
-      BottomGoalBar:{
-        marginTop: 20,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        flex: 0.2,
-      },
-      StartWeightText:{
-        marginRight: 10,
-        marginTop: 2,
-        fontSize: 18,
-      },
-      EndWeightText:{
-        marginLeft: 10,
-        marginTop: 2,
-        fontSize: 18,
-      },
-      bottomContentButtons:{
-        width: "95%",
-        flex: 1,
-        alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-      },
-      bottomContentAddButton:{
-        flex: 1,
-        marginLeft: 35,
-        marginRight: 20,
-        height: '42%',
-        borderWidth: 3,
-        borderRadius: 20,
-        backgroundColor: '#D3D3D3',
-        alignItems: 'center',
-        justifyContent: 'space-around'
-      },
-      bottomContentEditButton:{
-        flex: 1,
-        marginLeft: 20,
-        marginRight: 35,
-        height: '42%',
-        borderWidth: 3,
-        borderRadius: 20,
-        backgroundColor: '#D3D3D3',
-        alignItems: 'center',
-        justifyContent: 'space-around'
-      },
-      bottomContentButtonText:{
-        fontWeight: 'bold',
-        fontSize: 20,
-      }
-    });
+  container: {
+    flex: 1,
+    backgroundColor: '#ececea',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  navBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    height: 80,
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+  },
+  navButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navButtonText: {
+    fontSize: 12,
+    marginTop: 5,
+  },
+  TopContent: {
+    marginTop: 60,
+    flex: 2,
+    marginBottom: 5,
+    width: "100%",
+    alignItems: 'center',
+  },
+  MidContent: {
+    flex: 4,
+    backgroundColor: "#bdbdc7",
+    borderRadius: 20,
+    width: 300,
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#d7d7d8',
+    elevation: 5,
+  },
+  chart: {
+    marginVertical: 10,
+    marginLeft: 5
+  },
+  BottomContent: {
+    flex: 2.5,
+  },
+  BottomGoalBar: {
+    width: 350,
+    margin: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flex: 0.2,
+  },
+  barChartText: {
+    marginRight: 10,
+    marginLeft: 15,
+    marginTop: 2,
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  bottomContentButtons: {
+    width: "95%",
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  bottomContentButton: {
+    flex: 1,
+    margin: 30,
+    height: 60,
+    borderWidth: 3,
+    borderRadius: 20,
+    backgroundColor: '#bdbdc7',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    borderColor: '#bcbcc9',
+    elevation: 7
+  },
+  bottomContentButtonText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  titleText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingBottom: 5,
+    paddingTop: 8
+  },
+});
 
 export default HomeScreen;
